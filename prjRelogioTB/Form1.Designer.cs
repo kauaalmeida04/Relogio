9af9bbd60298ca1d646d88842e27dbd3aeee480a
Kauã Almeida <100322183+kauaalmeida04@users.noreply.github.com>
2022-05-06 20:42:09 -0300

    Kauã
    .santos

---
+++ prjRelogioTB/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace prjRelogioTB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">verdade se for necessário descartar os recursos gerenciados; caso contrário, falso.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte do Designer - não modifique
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pbRelogio = new System.Windows.Forms.PictureBox();
+            this.relogio = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pbRelogio)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbRelogio
+            // 
+            this.pbRelogio.Location = new System.Drawing.Point(12, 12);
+            this.pbRelogio.Name = "pbRelogio";
+            this.pbRelogio.Size = new System.Drawing.Size(409, 364);
+            this.pbRelogio.TabIndex = 0;
+            this.pbRelogio.TabStop = false;
+            // 
+            // relogio
+            // 
+            this.relogio.Enabled = true;
+            this.relogio.Interval = 1000;
+            this.relogio.Tick += new System.EventHandler(this.relogio_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(433, 388);
+            this.Controls.Add(this.pbRelogio);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "RELOGIO";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbRelogio)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbRelogio;
+        private System.Windows.Forms.Timer relogio;
+    }
+}
+
---
+++ prjRelogioTB/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prjRelogioTB
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string caminho = Environment.CurrentDirectory +
+            "\\fundo.png";
+        Image fundo;
+        Graphics g;
+        int hora;
+        int min;
+        int seg;
+        int xseg = 0, yseg = 0;
+        int xmin = 0, ymin = 0;
+        int xhora = 0, yhora = 0;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            fundo = Image.FromFile(caminho);
+            pbRelogio.Image = fundo;
+            g = pbRelogio.CreateGraphics();
+            pbRelogio.Width = fundo.Width;
+            pbRelogio.Height = fundo.Height;
+        }
+
+        private void relogio_Tick(object sender, EventArgs e)
+        {
+            DateTime bios = DateTime.Now;
+            hora = bios.Hour;
+            min = bios.Minute;
+            seg = bios.Second;
+            DesenharPonteiroSegundo();
+        }
+
+        private void DesenharPonteiroSegundo()
+        {
+            int cx = pbRelogio.Width / 2;
+            int cy = pbRelogio.Height / 2;
+            int raio = 110;
+            double angulo = -90 + (seg * 6);
+            Pen caneta = new Pen(Color.White, 4);
+            g.DrawLine(caneta, cx, cy, cx + xseg,
+                cy + yseg);
+            double rad = Math.PI * angulo / 180;
+            xseg = (int)(raio * Math.Cos(rad));
+            yseg = (int)(raio * Math.Sin(rad));
+            caneta.Color = Color.Red;
+            g.DrawLine(caneta, cx, cy, cx + xseg,
+                cy + yseg);
+
+        }
+    }
+}
